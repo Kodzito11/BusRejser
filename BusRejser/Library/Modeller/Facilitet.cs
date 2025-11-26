e807204e3c140a92f0f3b5122b6c6fcd1448e923
Kodzito11 <Kos001@edu.zealand.dk>
2025-11-26 16:05:18 +0100

    Add project files.

---
+++ BusRejser/Library/Modeller/Facilitet.cs
@@ -0,0 +1,49 @@
+﻿namespace BusRejser.Library.Modeller
+{
+	public enum FacilitetType
+	{
+		Komfort,
+		Sikkerhed,
+		Underholdning,
+		Andet
+	}
+
+	public class Facilitet
+	{
+		public int Id { get; private set; }
+		public string Name { get; private set; }
+		public string Description { get; private set; }
+		public decimal ExtraPrice { get; private set; }
+		public bool IsActive { get; private set; }
+		public FacilitetType Type { get; private set; }
+
+		private Facilitet(string name, string description, decimal extraPrice, FacilitetType type, bool isActive)
+		{
+			Name = name;
+			Description = description;
+			ExtraPrice = extraPrice;
+			Type = type;
+			IsActive = isActive;
+
+		}
+
+		public static Facilitet Create(string name, string description, decimal extraPrice, FacilitetType type, bool isActive)
+		{
+			if (string.IsNullOrWhiteSpace(name))
+				throw new ArgumentException("Navn på facilitet kræves.");
+
+			if (extraPrice < 0) throw new ArgumentOutOfRangeException(nameof(extraPrice), "Extra pris kan ikke være negativ");
+
+			return new Facilitet(name, description, extraPrice, type, isActive);
+		}
+
+		public void UpdatePrice(decimal newPrice)
+		{
+			if (newPrice < 0) throw new ArgumentOutOfRangeException(nameof(newPrice), "Pris kan ikke være negativ.");
+
+			ExtraPrice = newPrice;
+		}
+
+		public void Deactivate() => IsActive = false;
+	}
+}
---
+++ BusRejser/Library/Repositories/BusRepository.cs
@@ -0,0 +1,11 @@
+﻿using BusRejser.Library.Modeller;
+
+namespace BusRejser.Library.Repositories
+{
+	public class BusRepository
+	{
+
+		public List<Bus> GetAll() {  return new List<Bus>(); }
+
+	}
+}
